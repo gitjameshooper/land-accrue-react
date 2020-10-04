2594ec3db6fb74a0e80b729c571adbee780fe8ef
gitjameshooper <gitjameshooper@github.com>
2020-10-04 14:23:24 -0500

    Frontend Basic views

---
+++ src/components/navbar.component.js
@@ -0,0 +1,28 @@
+import React, { Component } from 'react';
+import { Link } from 'react-router-dom';
+
+export default class Navbar extends Component {
+
+    render() {
+        return (
+            <nav className="navbar navbar-dark bg-dark navbar-expand-lg">
+				<Link to="/" className="navbar-brand">Home</Link>
+				
+				<div className="collapse navbar-collapse">
+					<ul className="navbar-nav mr-auto">
+						<li className="navbar-item">
+						 <Link to="/" className="navbar-link">Home</Link>
+						 </li>		
+						 <li className="navbar-item">
+						 <Link to="/user" className="navbar-link">User</Link>
+						 </li>		
+						 <li className="navbar-item">
+						 <Link to="/state" className="navbar-link">State</Link>
+						 </li>
+					</ul>
+				</div>
+
+			</nav>
+        )
+    }
+}
---
+++ src/components/state.component.js
@@ -0,0 +1,52 @@
+import React, { Component } from 'react';
+import axios from 'axios';
+
+export default class State extends Component {
+	constructor (props){
+		super(props);
+
+		this.onChangeState = this.onChangeState.bind(this);
+		this.state = {
+			states: [],
+			stateName : '',
+			abbv : '',
+			counties : ''
+		}
+
+	
+	}
+
+	onChangeState(e) {
+		this.setState({
+			stateName: e.target.value
+		});
+
+	}
+    
+    componentDidMount(){
+    	
+		axios.get('http://localhost:5000/states')
+		.then(res => {
+			if(res.data.length > 0){
+				console.log(res.data);
+				this.setState({states: res.data.map(state => state.name)});
+			}
+		 });
+    }
+
+    render() {
+        return (
+            <div className="container">
+            State Component {this.state.stateName}
+            <select required className="form-control" value={this.state.stateName} onChange={this.onChangeState}>
+            {
+                this.state.states.map(function (state) {
+                	return <option key={state} value={state}>{state}</option>
+                })
+            }
+
+            </select>
+            </div>
+        )
+    }
+}
