ec40901aa315200a28c9b0451f4f6e806c34ccd6
gitjameshooper <gitjameshooper@github.com>
2020-10-04 14:23:24 -0500

    Frontend Basic views

--- backend/routes/users.js
+++ backend/routes/users.js
@@ -10,13 +10,32 @@ router.route('/').get((req, res) => {
 router.route('/add').post((req,res) => {
 
 	const username = req.body.username;
-	const newUser = new User({username});
+	const password = req.body.password;
+	const newUser = new User({username, password});
 
 	newUser.save()
 	.then(() => res.json('User added'))
 	.catch(err => res.status(400).json(`Error: ${err}`))
 });
 
+router.route('/:id').get((req, res) => {
+	User.findById(req.params.id)
+		.then(user => res.json(user))
+		.catch(err => res.status(400).json(`Error: ${err}`));
+});
+
+router.route('/:id').delete((req, res) => {
+	User.findByIdAndDelete(req.params.id)
+		.then(user => res.json('User Deleted'))
+		.catch(err => res.status(400).json(`Error: ${err}`));
+});
+
+router.route('/:id').patch((req, res) => {
+
+	User.findByIdAndUpdate(req.params.id, {$set: req.body})
+	     .then(() => res.json('User Updated'))
+		.catch(err => res.status(400).json(`Error: ${err}`));
+});
 
 
 module.exports = router;
