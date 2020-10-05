95e6879651f8b27b06458b618db9d7126d99ef14
gitjameshooper <gitjameshooper@github.com>
2020-10-04 20:45:51 -0500

    Add Fontawesome Icon

--- src/components/header/header.component.js
+++ src/components/header/header.component.js
@@ -1,6 +1,8 @@
 import React, { Component } from 'react';
 import { Link } from 'react-router-dom';
 import './header.scss';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faBars } from '@fortawesome/free-solid-svg-icons'
 
 export default class Header extends Component {
 
@@ -8,7 +10,7 @@ export default class Header extends Component {
         return (
             <header>
             <nav className="navbar navbar-dark bg-dark navbar-expand-lg">
-				<Link to="/" className="navbar-brand">Home</Link>
+				<Link to="/" className="navbar-brand"><FontAwesomeIcon icon={faBars} /></Link>
 				
 				<div className="collapse navbar-collapse">
 					<ul className="navbar-nav mr-auto">
