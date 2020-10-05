6c94c7b29f6380e189a7b33a7699827ef880f664
gitjameshooper <gitjameshooper@github.com>
2020-10-05 00:05:23 -0500

    Basic Redux Setup

--- src/App.js
+++ src/App.js
@@ -1,16 +1,33 @@
 import React from 'react';
 import { BrowserRouter as Router, Route } from 'react-router-dom';
+import { Provider } from 'react-redux';
+import { createStore, applyMiddleware, compose } from 'redux';
+import reduxThunk from 'redux-thunk';
+import rootReducer from './reducers';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import  Header from './components/header/header.component';
 import  Home from './components/home/home.component';
 import  Login from './components/login/login.component';
 import  Comps from './components/comps/comps.component';
-
-
 import './App.scss';
+
+
+const initialState = {};
+const middleware = [reduxThunk];
+const store = createStore(
+  rootReducer,
+  initialState,
+  compose(
+    applyMiddleware(...middleware),
+    window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+  )
+  );
+
+
 
 function App() {
     return (
+      <Provider store={store}>
       <Router>
         <Header />
         <div className="container logo">
@@ -19,6 +36,7 @@ function App() {
           <Route path="/login" exact component={Login} />
         </div>
       </Router>
+      </Provider>
     );
 }
 
--- src/components/comps/comps.component.js
+++ src/components/comps/comps.component.js
@@ -1,52 +1,73 @@
 import React, { Component } from 'react';
 import axios from 'axios';
+import PropTypes from 'prop-types';
+import { connect } from 'react-redux';
+import { getStates } from '../../actions/state.actions';
 
-export default class State extends Component {
-	constructor (props){
-		super(props);
+class Comps extends Component {
+	// constructor (props){
+	// 	super(props);
 
-		this.onChangeState = this.onChangeState.bind(this);
-		this.state = {
-			states: [],
-			stateName : '',
-			abbv : '',
-			counties : ''
-		}
+		// this.onChangeState = this.onChangeState.bind(this);
+		// this.state = {
+		// 	states: [],
+		// 	stateName : '',
+		// 	abbv : '',
+		// 	counties : ''
+		// }
 
 	
-	}
+	// }
 
-	onChangeState(e) {
-		this.setState({
-			stateName: e.target.value
-		});
+	// onChangeState(e) {
+	// 	this.setState({
+	// 		stateName: e.target.value
+	// 	});
 
-	}
-    
+	// }
+ 
     componentDidMount(){
+    	this.props.getStates();
     	
-		axios.get('http://localhost:5000/states')
-		.then(res => {
-			if(res.data.length > 0){
-				console.log(res.data);
-				this.setState({states: res.data.map(state => state.name)});
-			}
-		 });
+		// axios.get('http://localhost:5000/states')
+		// .then(res => {
+		// 	if(res.data.length > 0){
+		// 		console.log(res.data);
+		// 		this.setState({states: res.data.map(state => state.name)});
+		// 	}
+		//  });
     }
 
     render() {
+    	// const states = this.props.states;
+    	const states = this.props.states.map(state => (
+    		<div><p key={state.id}>{state.name}</p><p>{state.abbv}</p></div>
+        ));
         return (
             <div className="container comps-component">
-            CompsComponent {this.state.stateName}
-            <select required className="form-control" value={this.state.stateName} onChange={this.onChangeState}>
-            {
-                this.state.states.map(function (state) {
-                	return <option key={state} value={state}>{state}</option>
-                })
-            }
-
-            </select>
+             Comps Component
+            {states}
+             
+     
             </div>
+            
         )
     }
 }
+       // <select required className="form-control" value={this.state.stateName} onChange={this.onChangeState}>
+       //      {
+       //          this.state.states.map(function (state) {
+       //          	return <option key={state} value={state}>{state}</option>
+       //          })
+       //      }
+ 
+       //      </select>
+       // States.PropTypes = {
+       // 	getStates: PropTypes.func.isRequired,
+       // 	states: PropTypes.array.isRequired
+       // }
+       const mapStateToProps = state => ({
+       	 states: state.states.states
+       })
+
+export default connect(mapStateToProps, { getStates })(Comps);
