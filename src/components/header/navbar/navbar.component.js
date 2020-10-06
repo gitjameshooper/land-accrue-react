8e3a2503d514f3d5cee467260aede9561163ef68
gitjameshooper <gitjameshooper@github.com>
2020-10-06 18:37:56 -0500

    Style Navbar

--- src/components/header/navbar/navbar.component.js
+++ src/components/header/navbar/navbar.component.js
@@ -3,6 +3,9 @@ import { Link } from 'react-router-dom';
 import Button from '@material-ui/core/Button';
 import MenuIcon from '@material-ui/icons/Menu';
 import CloseIcon  from '@material-ui/icons/Close';
+import HomeIcon from '@material-ui/icons/Home';
+import SupervisorAccountIcon from '@material-ui/icons/SupervisorAccount';
+import LandscapeIcon from '@material-ui/icons/Landscape';
 import './navbar.scss';
 
 export default function NavBar() {
@@ -12,19 +15,18 @@ export default function NavBar() {
 
  
   return (
-    <div>
     <div className="navbar">
        <Link to="#" className="menu-bars"> <MenuIcon onClick={showSideBar} /> </Link>
-    </div>
-    <nav className={sideBar ? 'nav-menu active' : 'nav-menu'}>
-      <ul className="nav-menu-items" onClick={showSideBar} >
-        <li className="navbar-toggle nav-text"><CloseIcon /></li>
-        <li className="nav-text"><Link to="/" className="navbar-link">Home</Link></li>
-        <li className="nav-text"><Link to="/login" className="navbar-link">Admin Login </Link></li>
-        <li className="nav-text"><Link to="/land" className="navbar-link">Land</Link></li>
-        </ul>
 
-    </nav>
+      <nav className={sideBar ? 'nav-menu active' : 'nav-menu'}>
+        <ul className="nav-menu-items" onClick={showSideBar} >
+          <li className="navbar-toggle nav-text"><CloseIcon /></li>
+          <li className="nav-text"><Link to="/" className="navbar-link"><span><HomeIcon /></span>Home</Link></li>
+          <li className="nav-text"><Link to="/login" className="navbar-link"><span><SupervisorAccountIcon /></span>Admin Login </Link></li>
+          <li className="nav-text"><Link to="/land" className="navbar-link"><span><LandscapeIcon /></span>Land</Link></li>
+          </ul>
+
+      </nav>
     </div>
   );
 }
