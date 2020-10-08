ee37d17cef8a94147cf8d270e900291e0094804c
gitjameshooper <gitjameshooper@github.com>
2020-10-08 14:17:08 -0500

    Upload CSV files error handling

--- src/components/land/upload/upload.component.js
+++ src/components/land/upload/upload.component.js
@@ -1,4 +1,4 @@
-import React, { Component } from 'react';
+import React, { Component, useState } from 'react';
 import { makeStyles } from '@material-ui/core/styles';
 import InputLabel from '@material-ui/core/InputLabel';
 import MenuItem from '@material-ui/core/MenuItem';
@@ -17,11 +17,15 @@ export default class Upload extends Component {
             soldLandFile: null,
             usStates: USstates,
             usState: 'TX',
-            countyName: ''
+            countyName: '',
+            countyNameErr: '',
+            buyLandFileErr: '',
+            soldLandFileErr: ''
         }
         this.onChangeCounty = this.onChangeCounty.bind(this);
         this.onChangeState = this.onChangeState.bind(this);
         this.onChangeFile = this.onChangeFile.bind(this);
+        this.onSubmitFiles = this.onSubmitFiles.bind(this);
 
     }
     onChangeState(e) {
@@ -29,44 +33,61 @@ export default class Upload extends Component {
     }
     onChangeCounty(e) {
         this.setState({ countyName: e.target.value });
+        this.setState({ countyNameErr: ''});
     }
-   onChangeFile(e) { 
-     if(e.target.name === 'buyLand') this.setState({ buyLandFile: e.target.files[0] }); 
-     if(e.target.name === 'soldLand')  this.setState({ soldLandFile: e.target.files[0] });      
-    }; 
-      // On file upload (click the upload button) 
-    onSubmitFiles = (e) => { 
+    onChangeFile(e) {
+        if (e.target.name === 'buyLand') this.setState({ buyLandFile: e.target.files[0] }); this.setState({ buyLandFileErr: ''});
+        if (e.target.name === 'soldLand') this.setState({ soldLandFile: e.target.files[0] }); this.setState({ soldLandFileErr: ''});
+    };
+    // On file upload (click the upload button) 
+    onSubmitFiles = (e) => {
         e.preventDefault();
-      // Create an object of formData 
-     const formData = new FormData(); 
-     const config = {     
-        headers: { 'content-type': 'multipart/form-data' }
-      }
-       formData.append('county', this.state.countyName); 
-       formData.append('state', this.state.usState); 
-      // Update the formData object 
-      formData.append( 
-        'buy.csv', 
-        this.state.buyLandFile
-      );
+        // Error Checking for all fields
+        const isValid = () => {
+            let isValid = true;
+            if(!this.state.buyLandFile) {
+              this.setState({ buyLandFileErr: 'Please Add a Buy Land File'});
+              isValid = false;
+            }
+            if(!this.state.soldLandFile) {
+              this.setState({ soldLandFileErr: 'Please Add a Sold Land File'});
+              isValid = false;
+            }
+            // County 
+            if (this.state.countyName.trim().length < 3) {
+                this.setState({ countyNameErr: 'County Name is too short'});
+                isValid = false;
+            }
+            return isValid;
+        }
 
-      formData.append( 
-        'sold.csv', 
-        this.state.soldLandFile
-      );
-     
-      // Details of the uploaded file 
-      console.log(this.state); 
-     
-      // Request made to the backend api 
-      // Send formData object 
-      axios.post("http://localhost:5000/uploads/csv", formData, config).then(response => {
-            console.log(response);
-        })
-        .catch(error => {
-            console.log(error);
-        }); 
-    }; 
+
+        if (isValid()) {
+            // Create an object of formData 
+            const formData = new FormData();
+            const config = {
+                headers: { 'content-type': 'multipart/form-data' }
+            }
+            formData.append('county', this.state.countyName);
+            formData.append('state', this.state.usState);
+            // Update the formData object 
+            formData.append(
+                'buy.csv',
+                this.state.buyLandFile
+            );
+            formData.append(
+                'sold.csv',
+                this.state.soldLandFile
+            );
+            // Send Files to backend API
+            axios.post("http://localhost:5000/uploads/csv", formData, config).then(response => {
+                    console.log(response);
+                })
+                .catch(error => {
+                    console.log(error);
+                });
+        }
+    };
 
 
     render() {
@@ -79,13 +100,14 @@ export default class Upload extends Component {
             <div className="upload-component row">
 
               <h3>Upload CSV Files</h3>
-              <form onSubmit={this.onSubmitFiles}>
-               <select id="us-state" className="form-control" required onChange={this.onChangeState} value={this.state.usState}>
+              <form noValidate onSubmit={this.onSubmitFiles}>
+               <select id="us-state" className="form-control" onChange={this.onChangeState} value={this.state.usState}>
                 {usStates}
               </select>
-                <input type="text" className="form-control" required onChange={this.onChangeCounty} name={this.state.countyName}  placeholder="County Name" aria-describedby="basic-addon2"/>
-               <label>Buy Land CSV <br /><input type="file" required id="buy-land" name="buyLand" accept=".csv" onChange={this.onChangeFile} /> </label>
-               <label>Sold Land CSV <br /><input type="file" required id="sold-land" name="soldLand" accept=".csv" onChange={this.onChangeFile} /> </label>
+                <input type="text" className="form-control" onChange={this.onChangeCounty} name={this.state.countyName}  placeholder="County Name" aria-describedby="basic-addon2"/>
+               <span className="error">*{this.state.countyNameErr}</span>
+               <label>Buy Land CSV <br /><span className="error">*{this.state.soldLandFileErr}</span><input type="file" id="buy-land" name="buyLand" accept=".csv" onChange={this.onChangeFile} /> </label>
+               <label>Sold Land CSV <br /><span className="error">*{this.state.buyLandFileErr}</span><input type="file" id="sold-land" name="soldLand" accept=".csv" onChange={this.onChangeFile} /> </label>
             
                 <button type="submit">
                   Upload Files
@@ -97,4 +119,3 @@ export default class Upload extends Component {
         )
     }
 }
-
