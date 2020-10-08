186ff05730fce4ee8f58199442e3534a7a7700d9
gitjameshooper <gitjameshooper@github.com>
2020-10-08 13:10:59 -0500

    Server Upload Files Working

--- src/components/land/upload/upload.component.js
+++ src/components/land/upload/upload.component.js
@@ -31,33 +31,32 @@ export default class Upload extends Component {
         this.setState({ countyName: e.target.value });
     }
    onChangeFile(e) { 
-     if(e.target.files[0].name === 'buy.csv') this.setState({ buyLandFile: e.target.files[0] }); 
-     if(e.target.files[0].name === 'sold.csv') this.setState({ soldLandFile: e.target.files[0] });      
+     if(e.target.name === 'buyLand') this.setState({ buyLandFile: e.target.files[0] }); 
+     if(e.target.name === 'soldLand')  this.setState({ soldLandFile: e.target.files[0] });      
     }; 
       // On file upload (click the upload button) 
-    onUploadFiles = () => { 
-     console.log(this.state);
+    onSubmitFiles = (e) => { 
+        e.preventDefault();
       // Create an object of formData 
      const formData = new FormData(); 
      const config = {     
         headers: { 'content-type': 'multipart/form-data' }
       }
        formData.append('county', this.state.countyName); 
+       formData.append('state', this.state.usState); 
       // Update the formData object 
       formData.append( 
-        this.state.buyLandFile.name, 
+        'buy.csv', 
         this.state.buyLandFile
       );
 
-      // formData.append( 
-      //   this.state.soldLandFile.name, 
-      //   this.state.soldLandFile
-      // );
-     
-    
+      formData.append( 
+        'sold.csv', 
+        this.state.soldLandFile
+      );
      
       // Details of the uploaded file 
-      console.log(this.state.buyLandFile); 
+      console.log(this.state); 
      
       // Request made to the backend api 
       // Send formData object 
@@ -80,23 +79,19 @@ export default class Upload extends Component {
             <div className="upload-component row">
 
               <h3>Upload CSV Files</h3>
-              
-               <select id="us-state" className="form-control" onChange={this.onChangeState} value={this.state.usState}>
+              <form onSubmit={this.onSubmitFiles}>
+               <select id="us-state" className="form-control" required onChange={this.onChangeState} value={this.state.usState}>
                 {usStates}
               </select>
-              
-
-                <input type="text" className="form-control" onChange={this.onChangeCounty} name={this.state.countyName}  placeholder="County Name" aria-describedby="basic-addon2"/>
-
-               <label>Buy Land CSV <br /><input type="file" id="buy-land" name="buyLand" accept=".csv" onChange={this.onChangeFile} /> </label>
-
-
-               <label>Sold Land CSV <br /><input type="file" id="sold-land" name="soldLand" accept=".csv" onChange={this.onChangeFile} /> </label>
+                <input type="text" className="form-control" required onChange={this.onChangeCounty} name={this.state.countyName}  placeholder="County Name" aria-describedby="basic-addon2"/>
+               <label>Buy Land CSV <br /><input type="file" required id="buy-land" name="buyLand" accept=".csv" onChange={this.onChangeFile} /> </label>
+               <label>Sold Land CSV <br /><input type="file" required id="sold-land" name="soldLand" accept=".csv" onChange={this.onChangeFile} /> </label>
             
-                <button onClick={this.onUploadFiles}> 
+                <button type="submit">
                   Upload Files
                 </button> 
                 <p>Note: You can only upload .csv files</p>
+                </form>
 
             </div>
         )
