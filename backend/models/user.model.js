0105a965cf618425f706cfe5d6fa3958d7389d22
gitjameshooper <gitjameshooper@github.com>
2020-10-12 15:45:13 -0500

    Merge Data working

--- backend/models/user.model.js
+++ backend/models/user.model.js
@@ -4,31 +4,32 @@ const Schema = mongoose.Schema;
 
 const userSchema = new Schema({
 
-	name: {
-		type: String,
-		required: true,
-		trim: true,
-		minlength: 1
-	},
-	password: {
-		type: String,
-		required: true,
-		trim: true,
-		minlength: 1
-	},
-	email: {
-		type: String,
-		required: true,
-		trim: true,
-		unique: true,
-		minlength: 1
-	},
-	date: {
-		type: Date,
-		default: Date.now()
-	}
-},{
-	timestamp: true,	  
+    name: {
+        type: String,
+        required: true,
+        trim: true,
+        minlength: 1
+    },
+    password: {
+        type: String,
+        required: true,
+        trim: true,
+        minlength: 1
+    },
+    email: {
+        type: String,
+        required: true,
+        trim: true,
+        unique: true,
+        minlength: 1
+    },
+    date: {
+        type: Date,
+        default: Date.now()
+    }
+}, {
+    timestamp: true,
+    versionKey: false
 })
 
 module.exports = User = mongoose.model('User', userSchema);
