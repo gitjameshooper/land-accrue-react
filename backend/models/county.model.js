68522ab8596ac3145bd872a1d1e3251fe2e6516c
gitjameshooper <gitjameshooper@github.com>
2020-10-12 15:45:13 -0500

    Merge Data working

--- backend/models/county.model.js
+++ backend/models/county.model.js
@@ -244,7 +244,7 @@ const buyProperty = new Schema({
         unique: false,
         trim: true
     }
-})
+}, { versionKey: false });
 
 const soldProperty = new Schema({
     'ADDRESS': {
@@ -319,16 +319,325 @@ const soldProperty = new Schema({
         unique: false,
         trim: true
     }
-})
-const totalProperty = new Schema({
-    name: {
+}, { versionKey: false});
+const soldCompSchema = new Schema({
+	        "ADDRESS": {
         type: String,
         require: true,
         unique: false,
-        trim: true,
-        minlength: 1
+        trim: true
+    },
+        "CITY": {
+        type: String,
+        require: true,
+        unique: false,
+        trim: true
+    },
+        "STATE": {
+        type: String,
+        require: true,
+        unique: false,
+        trim: true
+    },
+        "ZIP": {
+        type: String,
+        require: true,
+        unique: false,
+        trim: true
+    },
+        "SOLD PRICE":  {
+        type: Number,
+        require: true,
+        unique: false,
+        trim: true
+    },
+        "LOT AREA":  {
+        type: Number,
+        require: true,
+        unique: false,
+        trim: true
+    },
+        "LOT ACREAGE":  {
+        type: Number,
+        require: true,
+        unique: false,
+        trim: true
+    },
+        "PRICE PER ACRE":  {
+        type: Number,
+        require: true,
+        unique: false,
+        trim: true
+    },
+        "LATITUDE": {
+        type: String,
+        require: true,
+        unique: false,
+        trim: true
+    },
+        "LONGITUDE": {
+        type: String,
+        require: true,
+        unique: false,
+        trim: true
+    },
+        "distance":  {
+        type: Number,
+        require: true,
+        unique: false,
+        trim: true
     }
-}, { versionKey: false })
+}, { versionKey: false });
+const totalProperty = new Schema({
+    "SITUS STREET ADDRESS": {
+        type: String,
+        require: true,
+        unique: false,
+        trim: true
+    },
+    "SITUS CITY": {
+        type: String,
+        require: true,
+        unique: false,
+        trim: true
+    },
+    "SITUS STATE": {
+        type: String,
+        require: true,
+        unique: false,
+        trim: true
+    },
+    "SITUS ZIP CODE": {
+        type: String,
+        require: true,
+        unique: false,
+        trim: true
+    },
+    "COUNTY": {
+        type: String,
+        require: true,
+        unique: false,
+        trim: true
+    },
+    "LOT AREA": {
+        type: Number,
+        require: true,
+        unique: false,
+        trim: true
+    },
+    "LOT ACREAGE": {
+        type: Number,
+        require: true,
+        unique: false,
+        trim: true
+    },
+    "LEGAL DESCRIPTION": {
+        type: String,
+        require: true,
+        unique: false,
+        trim: true
+    },
+    "LEGAL LOT": {
+        type: String,
+        require: true,
+        unique: false,
+        trim: true
+    },
+    "SUBDIVISION": {
+        type: String,
+        require: true,
+        unique: false,
+        trim: true
+    },
+    "MUNICIPALITY/TOWNSHIP": {
+        type: String,
+        require: true,
+        unique: false,
+        trim: true
+    },
+    "LATITUDE": {
+        type: String,
+        require: true,
+        unique: false,
+        trim: true
+    },
+    "LONGITUDE": {
+        type: String,
+        require: true,
+        unique: false,
+        trim: true
+    },
+    "APN - FORMATTED": {
+        type: String,
+        require: true,
+        unique: false,
+        trim: true
+    },
+    "IN FLOOD ZONE": {
+        type: String,
+        require: true,
+        unique: false,
+        trim: true
+    },
+    "OWNER MAILING NAME": {
+        type: String,
+        require: true,
+        unique: false,
+        trim: true
+    },
+    "MAILING STREET ADDRESS": {
+        type: String,
+        require: true,
+        unique: false,
+        trim: true
+    },
+    "MAIL CITY": {
+        type: String,
+        require: true,
+        unique: false,
+        trim: true
+    },
+    "MAIL STATE": {
+        type: String,
+        require: true,
+        unique: false,
+        trim: true
+    },
+    "MAIL ZIPZIP4": {
+        type: String,
+        require: true,
+        unique: false,
+        trim: true
+    },
+    "MARKET TOTAL VALUE":  {
+        type: Number,
+        require: true,
+        unique: false,
+        trim: true
+    },
+    "MARKET IMPROVEMENT VALUE":  {
+        type: Number,
+        require: true,
+        unique: false,
+        trim: true
+    },
+    "date":  {
+        type: String,
+        require: true,
+        unique: false,
+        trim: true
+    },
+    "pricePerAcre":  {
+        type: Number,
+        require: true,
+        unique: false,
+        trim: true
+    },
+    "avgPPA":  {
+        type: Number,
+        require: true,
+        unique: false,
+        trim: true
+    },
+    "avgPPA2":  {
+        type: Number,
+        require: true,
+        unique: false,
+        trim: true
+    },
+    "avgPPA3":  {
+        type: Number,
+        require: true,
+        unique: false,
+        trim: true
+    },
+    "estValue":  {
+        type: Number,
+        require: true,
+        unique: false,
+        trim: true
+    },
+    "estValue2":  {
+        type: Number,
+        require: true,
+        unique: false,
+        trim: true
+    },
+    "estValue3":  {
+        type: Number,
+        require: true,
+        unique: false,
+        trim: true
+    },
+    "offer":  {
+        type: Number,
+        require: true,
+        unique: false,
+        trim: true
+    },
+    "offer1":  {
+        type: Number,
+        require: true,
+        unique: false,
+        trim: true
+    },
+    "offer2":  {
+        type: Number,
+        require: true,
+        unique: false,
+        trim: true
+    },
+    "offer3":  {
+        type: Number,
+        require: true,
+        unique: false,
+        trim: true
+    },
+    "offerPPA":  {
+        type: Number,
+        require: true,
+        unique: false,
+        trim: true
+    },
+    "jasonOffer":  {
+        type: Number,
+        require: true,
+        unique: false,
+        trim: true
+    },
+    "jasonEstValue":  {
+        type: Number,
+        require: true,
+        unique: false,
+        trim: true
+    },
+    "statusColor": {
+        type: String,
+        require: true,
+        unique: false,
+        trim: true
+    },
+    "marketValueFlag":  {
+        type: Boolean,
+        require: true,
+        unique: false,
+        trim: true
+    },
+    "propertyLink": {
+        type: String,
+        require: true,
+        unique: false,
+        trim: true
+    },
+    "estimatedValue":  {
+        type: Number,
+        require: true,
+        unique: false,
+        trim: true
+    },
+    "soldArr": [soldCompSchema]
+}, { versionKey: false });
+
 
 
 const countySchema = new Schema({
@@ -348,15 +657,15 @@ const countySchema = new Schema({
         trim: true,
         minlength: 2
     },
-    buy: {
-        type: [buyProperty],
+    // buy: {
+    //     type: [buyProperty],
 
-    },
-    sold: {
-        type: [soldProperty],
+    // },
+    // sold: {
+    //     type: [soldProperty],
 
-    },
-    total: {
+    // },
+    totalProperties: {
         type: [totalProperty],
 
     },
--- backend/routes/uploads.js
+++ backend/routes/uploads.js
@@ -33,6 +33,7 @@ router.post('/csv', upload.fields([{ name: 'buy.csv', maxCount: 1 }, { name: 'so
     addData(usStateName, usStateAbbv, countyName);
 
     // Add error handling
+
 })
 
 
@@ -69,15 +70,18 @@ function getCountyId(usState, countyName) {
     });
 }
 
-function addProperties(type, usCounty, data) {
+function addProperties(name, usCounty, data) {
     return new Promise((res, rej) => {
         let properties = [];
+
         data.forEach((property) => {
+
             properties.push(property);
         })
-        usCounty[type] = properties;
+        usCounty[name] = properties;
         usCounty.save((err, result) => {
-            console.log(err);
+        	console.log('Total props added');
+			console.log(err);
             res(result);
         });
     });
@@ -184,6 +188,178 @@ function formatBuyData(csv) {
 }
 
 
+function distance(lat1, lon1, lat2, lon2, unit) {
+    if ((lat1 == lat2) && (lon1 == lon2)) {
+        return 0;
+    } else {
+        let radlat1 = Math.PI * lat1 / 180,
+            radlat2 = Math.PI * lat2 / 180,
+            theta = lon1 - lon2,
+            radtheta = Math.PI * theta / 180,
+            dist = Math.sin(radlat1) * Math.sin(radlat2) + Math.cos(radlat1) * Math.cos(radlat2) * Math.cos(radtheta);
+        if (dist > 1) {
+            dist = 1;
+        }
+        dist = Math.acos(dist);
+        dist = dist * 180 / Math.PI;
+        dist = dist * 60 * 1.1515;
+        if (unit == "K") { dist *= 1.609344 }
+        if (unit == "N") { dist *= 0.8684 }
+
+        return dist;
+    }
+}
+
+function mergeData(buyData, soldData) {
+
+    return new Promise((res, rej) => {
+        let dupArr = [],
+            uniqueId = 1,
+            BD = [],
+            buyDataAll = [],
+            buyDataFlood = [],
+            buyDataNonFlood = [];
+        _.forEach(buyData, (bv, bk) => {
+            let soldArr = [],
+                totalPricePerAcre = 0;
+            buyData[bk]['id'] = uniqueId++;
+
+            // 8 miles radius from buy property - Loops through each mile adding to the sold array
+            _.forEach([0, 1, 2, 3, 4, 5, 6, 7, 8], (v, k) => {
+                _.forEach(soldData, (sv, sk) => {
+                    let soldProp = { ...soldData[sk] },
+                        soldDistance = distance(bv['LATITUDE'], bv['LONGITUDE'], sv['LATITUDE'], sv['LONGITUDE'], 'M'),
+                        minAcre,
+                        maxAcre;
+                    switch (true) {
+                        case buyData[bk]['LOT ACREAGE'] > 12:
+                            minAcre = buyData[bk]['LOT ACREAGE'] - 7;
+                            maxAcre = buyData[bk]['LOT ACREAGE'] + 7;
+                            break;
+                        case buyData[bk]['LOT ACREAGE'] > 8:
+                            minAcre = buyData[bk]['LOT ACREAGE'] - 4;
+                            maxAcre = buyData[bk]['LOT ACREAGE'] + 4;
+                            break;
+                        case buyData[bk]['LOT ACREAGE'] > 5:
+                            minAcre = buyData[bk]['LOT ACREAGE'] - 2;
+                            maxAcre = buyData[bk]['LOT ACREAGE'] + 2;
+                            break;
+                        case buyData[bk]['LOT ACREAGE'] > 3:
+                            minAcre = buyData[bk]['LOT ACREAGE'] - 1;
+                            maxAcre = buyData[bk]['LOT ACREAGE'] + 1;
+                            break;
+                        case buyData[bk]['LOT ACREAGE'] > .50:
+                            minAcre = buyData[bk]['LOT ACREAGE'] - .50;
+                            maxAcre = buyData[bk]['LOT ACREAGE'] + .50;
+                            break;
+
+                        default:
+                            minAcre = buyData[bk]['LOT ACREAGE'] - .25;
+                            maxAcre = buyData[bk]['LOT ACREAGE'] + .25;
+                            break;
+                    }
+
+                    // Less than 5 sold properties on array and Less than (d) miles away and close to the same lot acreage
+                    if (soldArr.length < 5 && soldDistance < v + 1 && soldDistance > v && minAcre < soldProp['LOT ACREAGE'] && maxAcre > soldProp['LOT ACREAGE']) {
+
+                        soldProp['distance'] = Math.round(soldDistance * 100) / 100;
+                        totalPricePerAcre += soldData[sk]['PRICE PER ACRE'];
+                        soldArr.push(soldProp);
+                    }
+                });
+            });
+            buyData[bk]['soldArr'] = soldArr;
+
+            // Top 3 Closests by distance: Properties Price per Acre
+            let sortedDistanceSoldArr = _.sortBy(soldArr, ['distance']),
+                totalPPADistance = 0,
+                totalPPADistanceDivider = 0;
+
+
+            if (sortedDistanceSoldArr[0] && sortedDistanceSoldArr[1] && sortedDistanceSoldArr[2]) {
+
+                totalPPADistance = sortedDistanceSoldArr[0]['PRICE PER ACRE'] + sortedDistanceSoldArr[1]['PRICE PER ACRE'] + sortedDistanceSoldArr[2]['PRICE PER ACRE'];
+                totalPPADistanceDivider = 3;
+            } else if (sortedDistanceSoldArr[0] && sortedDistanceSoldArr[1]) {
+                totalPPADistance = sortedDistanceSoldArr[0]['PRICE PER ACRE'] + sortedDistanceSoldArr[1]['PRICE PER ACRE'];
+                totalPPADistanceDivider = 2;
+            } else if (sortedDistanceSoldArr[0]) {
+                totalPPADistance = sortedDistanceSoldArr[0]['PRICE PER ACRE'];
+                totalPPADistanceDivider = 1;
+            }
+
+            buyData[bk]['avgPPA'] = totalPPADistance / totalPPADistanceDivider;
+
+
+            // 3 middle with avg. Price per Acre
+            let sortedPPASoldArr = _.sortBy(soldArr, ['PRICE PER ACRE']),
+                totalPPAMiddle = 0,
+                totalPPAMiddleDivider = 0;
+            if (sortedPPASoldArr[1] && sortedPPASoldArr[2] && sortedPPASoldArr[3]) {
+                totalPPAMiddle = sortedPPASoldArr[1]['PRICE PER ACRE'] + sortedPPASoldArr[2]['PRICE PER ACRE'] + sortedPPASoldArr[3]['PRICE PER ACRE'];
+                totalPPAMiddleDivider = 3;
+            } else if (sortedPPASoldArr[1] && sortedPPASoldArr[2]) {
+                totalPPAMiddle = sortedPPASoldArr[1]['PRICE PER ACRE'] + sortedPPASoldArr[2]['PRICE PER ACRE'];
+                totalPPAMiddleDivider = 2;
+            } else if (sortedPPASoldArr[1]) {
+                totalPPAMiddle = sortedPPASoldArr[1]['PRICE PER ACRE'];
+                totalPPAMiddleDivider = 1;
+            }
+            buyData[bk]['avgPPA2'] = totalPPAMiddle / totalPPAMiddleDivider;
+
+            // All 5 avg. Price per Acre
+            buyData[bk]['avgPPA3'] = totalPricePerAcre > 0 ? Math.round(totalPricePerAcre / soldArr.length) : 0;
+
+            // Flood Zone TRUE-> discount 25%
+            // if (buyData[bk]['IN FLOOD ZONE']) {
+            //     buyData[bk]['avgPPA'] *= .75;
+            //     buyData[bk]['avgPPA2'] *= .75;
+            //     buyData[bk]['avgPPA3'] *= .75;
+            // }
+
+            // Calculates Estimated Values and Offers
+            buyData[bk]['estValue'] = Math.round(buyData[bk]['avgPPA'] * buyData[bk]['LOT ACREAGE']);
+            buyData[bk]['estValue2'] = Math.round(buyData[bk]['avgPPA2'] * buyData[bk]['LOT ACREAGE'])
+            buyData[bk]['estValue3'] = Math.round(buyData[bk]['avgPPA3'] * buyData[bk]['LOT ACREAGE'])
+            buyData[bk]['offer1'] = Math.floor((buyData[bk]['estValue'] * .50) / 100) * 100;
+            buyData[bk]['offer2'] = Math.floor((buyData[bk]['estValue2'] * .50) / 100) * 100;
+            buyData[bk]['offer3'] = Math.floor((buyData[bk]['estValue3'] * .50) / 100) * 100;
+            buyData[bk]['offerPPA'] = Math.round(buyData[bk]['offer'] / buyData[bk]['LOT ACREAGE']);
+
+
+            // Add status color based off the amount of sold propeties
+            if (soldArr.length > 3) {
+                buyData[bk]['statusColor'] = 'green';
+                buyData[bk]['offer'] = buyData[bk]['offer2'];
+                buyData[bk]['estimatedValue'] = buyData[bk]['estValue2'];
+
+            } else if (soldArr.length <= 3 && soldArr.length > 1) {
+                buyData[bk]['statusColor'] = 'yellow';
+                buyData[bk]['offer'] = buyData[bk]['offer3']
+                buyData[bk]['estimatedValue'] = buyData[bk]['estValue3'];
+            } else {
+                buyData[bk]['statusColor'] = 'red';
+                buyData[bk]['offer'] = buyData[bk]['offer3']
+                buyData[bk]['estimatedValue'] = buyData[bk]['estValue3'];
+            }
+
+            // Calculate Price Per Acre
+            buyData[bk]['pricePerAcre'] = Math.round(buyData[bk]['estimatedValue'] / buyData[bk]['LOT ACREAGE']);
+            // MARKET TOTAL VALUE vs Estimated Value
+            buyData[bk]['marketValueFlag'] = (buyData[bk]['statusColor'] !== 'red') && (buyData[bk]['MARKET TOTAL VALUE'] / buyData[bk]['estValue']) > 2 ? true : false;
+
+            if (buyData[bk]['marketValueFlag']) {
+                buyData[bk]['statusColor'] = 'red';
+            }
+
+            buyDataAll[bk] = buyData[bk];
+
+        });
+        res(_.differenceBy(buyData, dupArr, 'id'));
+    });
+}
+
+
 
 async function addData(usStateName, usStateAbbv, countyName) {
     try {
@@ -216,10 +392,12 @@ async function addData(usStateName, usStateAbbv, countyName) {
                 usCounty = doc;
             });
         }
-        // Add properties into counties collection in DB
-        let buyD = await csvToJson().fromFile(`./csv/${usStateAbbv}/${countyName}/buy.csv`).then((data) => formatBuyData(data)).then((data) => addProperties('buy', usCounty, data));
-        let soldD = await csvToJson().fromFile(`./csv/${usStateAbbv}/${countyName}/sold.csv`).then((data) => formatSoldData(data)).then((data) => addProperties('sold', buyD, data));
-        return soldD;
+        // Add Buy and sold properties into counties collection in DB
+        let buyData = await csvToJson().fromFile(`./csv/${usStateAbbv}/${countyName}/buy.csv`).then((data) => formatBuyData(data));
+        let soldData = await csvToJson().fromFile(`./csv/${usStateAbbv}/${countyName}/sold.csv`).then((data) => formatSoldData(data));
+        let totalData = await mergeData(buyData, soldData).then((data) => addProperties('totalProperties', usCounty, data));
+
+        return totalData;
     } catch (e) {
         console.log(e);
     } finally {
