dceec066d63a93cd231a6a50f07b6629e89c50fc
gitjameshooper <gitjameshooper@github.com>
2020-10-25 16:32:49 -0500

    Style About Page

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React, { Component } from 'react';
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import Header from './components/header/header.component';
-import Home from './components/home/home.component';
+import About from './components/about/about.component';
 import Login from './components/login/login.component';
 import Land from './components/land/land.component';
 import Container from '@material-ui/core/Container';
@@ -17,7 +17,7 @@ class App extends Component {
         <Header />
         <Container component="main" maxWidth="lg">
         <Switch>
-          <Route path="/" exact component={Home} />
+          <Route path="/" exact component={About} />
           <Route path="/land" exact component={Land} />
           <Route path="/login" exact component={Login} />
           </Switch>
