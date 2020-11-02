085c50eb1c63ac676275132ce1cf3985283c9eeb
gitjameshooper <gitjameshooper@github.com>
2020-11-02 14:21:32 -0600

    Fix small errors

--- src/components/header/header.component.js
+++ src/components/header/header.component.js
@@ -1,21 +1,13 @@
-import React, { Component, useState, useContext } from 'react';
-import { Context } from './../../store';
+import React from 'react';
 import './header.scss';
 import NavBar from './navbar/navbar.component';
 
 
 export default function Header(props) {
 
-    const [storeState, setStoreState] = useContext(Context);
-
-
-
     return (
-         
         <header>
-
-        <NavBar />
-
-    </header>
+        	<NavBar />
+    	</header>
     );
 }
