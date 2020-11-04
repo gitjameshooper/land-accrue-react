a70000e9620c10732784af408788824f30d760f7
gitjameshooper <gitjameshooper@github.com>
2020-11-03 20:45:34 -0600

    Styled Table

--- src/components/land/info/info.component.js
+++ src/components/land/info/info.component.js
@@ -1,10 +1,19 @@
 import React from 'react';
+import LabelIcon from '@material-ui/icons/Label';
+import './info.scss';
+
+export default function Info(props) {
 
 
 
-export default function Info(props){
-
-
-
-	return (<span>Info. here  Red, Green, Yellow</span>);
+    return (
+        <div className="info-component">
+			<h3>Information</h3>
+			<ul>
+				<li className="green"><LabelIcon /> Green</li>
+				<li className="yellow"><LabelIcon /> Yellow</li>
+				<li className="red"><LabelIcon /> Red</li>
+			</ul>
+		</div>
+    );
 }
