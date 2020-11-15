8b8c52de3035537aa6ffd8ed8c4bf04e8ca3d2de
gitjameshooper <gitjameshooper@github.com>
2020-11-15 13:33:20 -0600

    Update About Spacing

--- backend/routes/us-states.js
+++ backend/routes/us-states.js
@@ -8,21 +8,4 @@ router.get("/", (req, res) => {
     .catch((err) => res.status(400).json({ status: false, msg: err }));
 });
 
-// router.post('/add', auth, (req, res) => {
-// 	const state = req.body.state;
-// 	const abbv = req.body.abbv;
-// 	const county = req.body.county;
-
-// 	const newState = new USState({
-// 		state,
-// 		abbv,
-// 		county
-// 	});
-
-// 	newState.save()
-// 	.then(() => res.json('US State Added'))
-// 	.catch(err => res.status(400).json(`Error: ${err}`))
-
-// });
-
 module.exports = router;
