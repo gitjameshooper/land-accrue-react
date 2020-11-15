294f3e1ea53b8e402407c3cf9691626b20ee26de
gitjameshooper <gitjameshooper@github.com>
2020-11-15 00:17:30 -0600

    Finished Info section

--- src/components/land/info/info.component.js
+++ src/components/land/info/info.component.js
@@ -6,25 +6,44 @@ import "./info.scss";
 export default function Info(props) {
   return (
     <Grid container component="section" className="info-component" spacing={2}>
-      <h4>Information</h4>
-      <Grid item xs={12} sm={6} className="item-1">
+      <h3>Information</h3>
+      <Grid item xs={12} sm={4} className="item-1">
+        <h4>Status Ranking</h4>
         <ul>
           <li className="green">
-            <LabelIcon /> Green<p>4+ Sold Properties found giving the most reliable price estimate</p>
+            <LabelIcon /> <span className="status-title">Green</span>
+            <span className="description"> (4+) Sold Properties found giving the most reliable price estimate</span>
           </li>
           <li className="yellow">
-            <LabelIcon /> Yellow<p>2-3 Sold Properties found giving less reliable price estimate</p>
+            <LabelIcon /> <span className="status-title">Yellow</span>
+            <span className="description">(2-3) Sold Properties found giving less reliable price estimate</span>
           </li>
           <li className="red">
-            <LabelIcon /> Red<p>0-1 Sold Properties found giving a no reliable price estimate</p>
+            <LabelIcon /> <span className="status-title">Red</span>
+            <span className="description">(0-1) Sold Properties found giving a no reliable price estimate</span>
           </li>
         </ul>
       </Grid>
-      <Grid item xs={12} sm={6} className="item-2">
+      <Grid item xs={12} sm={4} className="item-2">
+        <h4>Estimated Values</h4>
+        <ul>
+          <li>
+            <b>Estimate 1</b> is calculated by averaging the top 3 closests sold properties by distance
+          </li>
+          <li>
+            <b>Estimate 2</b> is calculated by sorting with price per acre then averaging the middle 3
+          </li>
+          <li>
+            <b>Estimate 3</b> is calculated by averaging all 5 properties
+          </li>
+        </ul>
+      </Grid>
+      <Grid item xs={12} sm={4} className="item-3">
         <h4>Quick Notes</h4>
         <ul>
-          <li>All data uploaded will overwrite previous CVS data for that state and county</li>
-          <li>Estimated values calculation</li>
+          <li>All data uploaded will overwrite previous CSV data for that state and county</li>
+          <li>You can only upload CSV files</li>
+          <li>You must be logged in as an admin to upload files or change the final offer price</li>
         </ul>
       </Grid>
     </Grid>
