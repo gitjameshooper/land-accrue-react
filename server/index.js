cca8b0b250d4fd1e7a7dbe12e574402e09008e26
gitjameshooper <jamesthomashooper@gmail.com>
2020-11-21 15:40:47 -0600

    Change path again

--- server/index.js
+++ server/index.js
@@ -19,9 +19,9 @@ mongoose.connect(uri, {
 });
 // Serve static assets in production
 if (process.env.NODE_ENV === "production") {
-  app.use(express.static("client/build"));
+  app.use(express.static("/client/build"));
   app.get("*", (req, res) => {
-    res.sendFile(path.resolve(__dirname, "client/build", "index.html"));
+    res.sendFile(path.resolve(__dirname, "/client/build", "index.html"));
   });
 }
 
